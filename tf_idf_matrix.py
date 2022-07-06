bc3d1073d3dfdb5378ada4be443ddcfef127392d
Justin <justin13601@hotmail.com>
2022-07-06 12:25:25 -0400

    07/06/22 - deployment: define ngrams class for tf_idf_matrix.ngrams

--- tf_idf_matrix.py
+++ tf_idf_matrix.py
@@ -14,24 +14,28 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-# set ngram N:
-def ngrams(string, n=10):
-    """
-    Takes an input string, cleans it and converts to ngrams.
-    """
-    string = str(string)
-    string = string.lower()  # lower case
-    string = fix_text(string)  # fix text
-    string = string.encode("ascii", errors="ignore").decode()  # remove non ascii chars
-    chars_to_remove = [")", "(", ".", "|", "[", "]", "{", "}", "'", "-"]
-    rx = '[' + re.escape(''.join(chars_to_remove)) + ']'  # remove punc, brackets etc...
-    string = re.sub(rx, '', string)
-    string = string.replace('&', 'and')
-    string = string.title()  # normalise case - capital at start of each word
-    string = re.sub(' +', ' ', string).strip()  # get rid of multiple spaces and replace with a single
-    string = ' ' + string + ' '  # pad names for ngrams...
-    ngrams = zip(*[string[i:] for i in range(n)])
-    return [''.join(ngram) for ngram in ngrams]
+class ngrams_class:
+    def __init__(self):
+        return
+
+    # set ngram N:
+    def ngrams(self, string, n=10):
+        """
+        Takes an input string, cleans it and converts to ngrams.
+        """
+        string = str(string)
+        string = string.lower()  # lower case
+        string = fix_text(string)  # fix text
+        string = string.encode("ascii", errors="ignore").decode()  # remove non ascii chars
+        chars_to_remove = [")", "(", ".", "|", "[", "]", "{", "}", "'", "-"]
+        rx = '[' + re.escape(''.join(chars_to_remove)) + ']'  # remove punc, brackets etc...
+        string = re.sub(rx, '', string)
+        string = string.replace('&', 'and')
+        string = string.title()  # normalise case - capital at start of each word
+        string = re.sub(' +', ' ', string).strip()  # get rid of multiple spaces and replace with a single
+        string = ' ' + string + ' '  # pad names for ngrams...
+        ngrams = zip(*[string[i:] for i in range(n)])
+        return [''.join(ngram) for ngram in ngrams]
 
 
 def load_config(file):
