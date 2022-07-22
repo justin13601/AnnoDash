9fb2267d0f37f0f12bab689bbbdeacf6cf34b520
Justin <justin13601@hotmail.com>
2022-07-21 23:08:24 -0400

    07/21/22 - use pip related_ontologies package for tf_idf

--- tf_idf_matrix.py
+++ tf_idf_matrix.py
@@ -14,26 +14,7 @@ from tqdm import tqdm
 from ftfy import fix_text
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
-
-# set ngram N:
-def ngrams(string, n=10):
-    """
-    Takes an input string, cleans it and converts to ngrams.
-    """
-    string = str(string)
-    string = string.lower()  # lower case
-    string = fix_text(string)  # fix text
-    string = string.encode("ascii", errors="ignore").decode()  # remove non ascii chars
-    chars_to_remove = [")", "(", ".", "|", "[", "]", "{", "}", "'", "-"]
-    rx = '[' + re.escape(''.join(chars_to_remove)) + ']'  # remove punc, brackets etc...
-    string = re.sub(rx, '', string)
-    string = string.replace('&', 'and')
-    string = string.title()  # normalise case - capital at start of each word
-    string = re.sub(' +', ' ', string).strip()  # get rid of multiple spaces and replace with a single
-    string = ' ' + string + ' '  # pad names for ngrams...
-    ngrams = zip(*[string[i:] for i in range(n)])
-    return [''.join(ngram) for ngram in ngrams]
+from related_ontologies.related import ngrams
 
 
 def load_config(file):
