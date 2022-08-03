594857c82dcf1ea333954c5371e3075fa600e317
Justin <justin13601@hotmail.com>
2022-08-03 18:09:58 -0400

    08/03/22 - SNOMED-CT loading

--- tests/test.py
+++ tests/test.py
@@ -29,19 +29,29 @@ def load_data(path):
 
 
 if __name__ == "__main__":
-    df_labitems = load_data('../demo-data/D_LABITEMS.csv')
-    df_labevents = load_data('../demo-data/LABEVENTS.csv')
+    # df_labitems = load_data('../demo-data/D_LABITEMS.csv')
+    # df_labevents = load_data('../demo-data/LABEVENTS.csv')
+    #
+    # print("Data Loaded.")
+    #
+    # labitems_list = df_labitems["label"].unique()
+    # labitems_dict = pd.Series(df_labitems.label.values, index=df_labitems.itemid.values).to_dict()
+    #
+    # categories = list(set(map(lambda x: x.lower().capitalize(), df_labitems["category"].unique().tolist())))
+    # categories = categories + list(set(map(lambda x: x.lower().capitalize(), df_labitems["fluid"].unique().tolist())))
+    #
+    # target = labitems_list[4]
+    #
+    # for itemid, label in labitems_dict.items():
+    #     if target == label:
+    #         df_table = df_labevents.query(f'itemid == {itemid}')
 
-    print("Data Loaded.")
+    snomed_dir = r'C:\Users\Justin\Documents\SickKids\MIMIC\SNOMED-CT\SnomedCT_USEditionRF2_PRODUCTION_20220301T120000Z\Snapshot\Terminology'
 
-    labitems_list = df_labitems["label"].unique()
-    labitems_dict = pd.Series(df_labitems.label.values, index=df_labitems.itemid.values).to_dict()
+    path_descriptions = os.path.join(snomed_dir, 'sct2_Description_Snapshot-en_US1000124_20220301.txt')
+    df_snomed_descriptions = pd.read_csv(path_descriptions, sep='\t')
 
-    categories = list(set(map(lambda x: x.lower().capitalize(), df_labitems["category"].unique().tolist())))
-    categories = categories + list(set(map(lambda x: x.lower().capitalize(), df_labitems["fluid"].unique().tolist())))
+    df_snomed_descriptions = df_snomed_descriptions.loc[df_snomed_descriptions['active'] == 1]
+    df_snomed_descriptions = df_snomed_descriptions.sort_values('effectiveTime').drop_duplicates('conceptId', keep='last')
 
-    target = labitems_list[4]
-
-    for itemid, label in labitems_dict.items():
-        if target == label:
-            df_table = df_labevents.query(f'itemid == {itemid}')
+    print("Done")
