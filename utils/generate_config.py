69cd9a73bf6b1d87d7f681b0eecefc1e6d05ee74
Justin Xu <justin13601@hotmail.com>
2022-08-26 23:42:10 -0400

    08/26/22 - updated generate_config.py with new format/names

--- utils/generate_config.py
+++ utils/generate_config.py
@@ -1,25 +1,16 @@
 import yaml
 from ml_collections import config_dict
 
-
-def load_config(file):
-    print(f'Loading {file}...')
-    with open(file, "r") as f:
-        configurations = yaml.safe_load(f)
-        print('Done.\n')
-        return configurations
-
-
 cfg = config_dict.ConfigDict()
 
 cfg.directories = config_dict.ConfigDict()
-cfg.directories.results = 'results-json'
+cfg.directories.results = 'results-json/demo'
 cfg.directories.concepts = config_dict.ConfigDict()
 cfg.directories.concepts.location = 'demo-data'
-cfg.directories.concepts.filename = 'D_LABITEMS.csv'
+cfg.directories.concepts.filename = 'demo_chartevents_user_1.csv'
 cfg.directories.data = config_dict.ConfigDict()
 cfg.directories.data.location = 'demo-data'
-cfg.directories.data.filename = 'LABEVENTS.csv'
+cfg.directories.data.filename = 'CHARTEVENTS.csv'
 
 cfg.ontology = config_dict.ConfigDict()
 cfg.ontology.location = 'ontology'
@@ -30,16 +21,16 @@ cfg.ontology.related.umls_apikey = config_dict.placeholder(str)
 cfg.ontology.related.umls_apikey = '1c3bfb9d-bcd1-472b-8ff5-d58adbb1c047'
 
 cfg.graphs = config_dict.ConfigDict()
-cfg.graphs.pairs = config_dict.ConfigDict()
-cfg.graphs.pairs.pair_one = config_dict.ConfigDict()
-cfg.graphs.pairs.pair_one.items = 50821, 50818
-cfg.graphs.pairs.pair_one.label = 'Blood Gas'
-cfg.graphs.pairs.pair_two = config_dict.ConfigDict()
-cfg.graphs.pairs.pair_two.items = 50912, 50971
-cfg.graphs.pairs.pair_two.label = 'Chemistry'
-cfg.graphs.pairs.pair_three = config_dict.ConfigDict()
-cfg.graphs.pairs.pair_three.items = 51222, 51300
-cfg.graphs.pairs.pair_three.label = 'Complete Blood Count'
+cfg.graphs.ref_vals = config_dict.ConfigDict()
+cfg.graphs.ref_vals.tab_one = config_dict.ConfigDict()
+cfg.graphs.ref_vals.tab_one.items = 223900, 223901, 220739
+cfg.graphs.ref_vals.tab_one.label = 'Glascow Coma Score'
+cfg.graphs.ref_vals.tab_two = config_dict.ConfigDict()
+cfg.graphs.ref_vals.tab_two.items = 223834, 223835, 220339, 224700
+cfg.graphs.ref_vals.tab_two.label = 'Ventilation'
+cfg.graphs.ref_vals.tab_three = config_dict.ConfigDict()
+cfg.graphs.ref_vals.tab_three.items = 220045, 220181, 220210, 220277, 223761
+cfg.graphs.ref_vals.tab_three.label = 'Vital Signs'
 cfg.graphs.kwargs = config_dict.ConfigDict()
 cfg.graphs.kwargs.title_font = 'verdana'
 cfg.graphs.kwargs.title_color = 'Black'
@@ -47,6 +38,7 @@ cfg.graphs.kwargs.title_size = 25
 cfg.graphs.kwargs.text_font = 'verdana'
 cfg.graphs.kwargs.text_color = 'Black'
 cfg.graphs.kwargs.text_size = 12
+cfg.graphs.kwargs.opacity = 0.3
 cfg.graphs.kwargs.height = 360
 cfg.graphs.kwargs.spikes = True
 
@@ -77,7 +69,7 @@ cfg.temp.mimic_iv_version = 2.0
 ########################################
 
 
-with open('../config.yaml', 'w') as yaml_file:
+with open('../config-demo.yaml', 'w') as yaml_file:
     yaml.dump(cfg, yaml_file)
 
 print(cfg)
