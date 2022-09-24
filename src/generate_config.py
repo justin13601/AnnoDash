668e2bfd48d281179215b475cf6a03024475602c
Justin Xu <justin13601@hotmail.com>
2022-09-23 23:57:28 -0400

    09/23/22 - new config format

--- src/generate_config.py
+++ src/generate_config.py
@@ -1,75 +1,62 @@
 import yaml
 from ml_collections import config_dict
 
-cfg = config_dict.ConfigDict()
+# configurations
+save_directory = 'results-json/demo'
+concepts_file = 'demo-data/demo_chartevents_user_1.csv'
+data_file = 'demo-data/CHARTEVENTS.csv'
+ontology_directory = 'ontology'
+ontology_search_method = 'pylucene'  # [sqlite, pylucene, tf-idf]
+# umls_apikey = '1c3bfb9d-bcd1-472b-8ff5-d58adbb1c047'
 
-cfg.directories = config_dict.ConfigDict()
-cfg.directories.results = 'results-json/demo'
-cfg.directories.concepts = config_dict.ConfigDict()
-cfg.directories.concepts.location = 'demo-data'
-cfg.directories.concepts.filename = 'demo_chartevents_user_1.csv'
-cfg.directories.data = config_dict.ConfigDict()
-cfg.directories.data.location = 'demo-data'
-cfg.directories.data.filename = 'CHARTEVENTS.csv'
-
-cfg.ontology = config_dict.ConfigDict()
-cfg.ontology.location = 'ontology'
-cfg.ontology.related = config_dict.ConfigDict()
-cfg.ontology.related.location = config_dict.placeholder(str)
-# cfg.ontology.related.location = 'ontology/related'
-cfg.ontology.related.umls_apikey = config_dict.placeholder(str)
-# cfg.ontology.related.umls_apikey = '1c3bfb9d-bcd1-472b-8ff5-d58adbb1c047'
-
-cfg.graphs = config_dict.ConfigDict()
-cfg.graphs.ref_vals = config_dict.ConfigDict()
-cfg.graphs.ref_vals.tab_one = config_dict.ConfigDict()
-cfg.graphs.ref_vals.tab_one.items = 223900, 223901, 220739
-cfg.graphs.ref_vals.tab_one.label = 'Glascow Coma Score'
-cfg.graphs.ref_vals.tab_two = config_dict.ConfigDict()
-cfg.graphs.ref_vals.tab_two.items = 223834, 223835, 220339, 224700
-cfg.graphs.ref_vals.tab_two.label = 'Ventilation'
-cfg.graphs.ref_vals.tab_three = config_dict.ConfigDict()
-cfg.graphs.ref_vals.tab_three.items = 220045, 220181, 220210, 220277, 223761
-cfg.graphs.ref_vals.tab_three.label = 'Vital Signs'
-cfg.graphs.kwargs = config_dict.ConfigDict()
-cfg.graphs.kwargs.title_font = 'verdana'
-cfg.graphs.kwargs.title_color = 'Black'
-cfg.graphs.kwargs.title_size = 25
-cfg.graphs.kwargs.text_font = 'verdana'
-cfg.graphs.kwargs.text_color = 'Black'
-cfg.graphs.kwargs.text_size = 12
-cfg.graphs.kwargs.opacity = 0.3
-cfg.graphs.kwargs.height = 360
-cfg.graphs.kwargs.spikes = True
-
-########################################
-
-cfg.temp = config_dict.ConfigDict()
-cfg.temp.mimic_iv_version = config_dict.placeholder(float)
-cfg.temp.mimic_iv_version = 2.0
-
-########################################
-# labels_dictionary = {
-#     'app': 'dash-web'
-# }
-#
-# metadata_dictionary = {
-#     'name': 'mimic-iv-dash-development',
-#     'labels': config_dict.FrozenConfigDict(labels_dictionary)
-# }
-#
-# cfg.metadata = config_dict.FrozenConfigDict(metadata_dictionary)
-#
-# temp_dictionary = {
-#     'test': True,
-#     'mimic_iv_version': 2.0
-# }
-#
-# cfg.temp = config_dict.FrozenConfigDict(temp_dictionary)
-########################################
+graph_title_font = 'verdana'
+graph_title_color = 'Black'
+graph_title_size = 25
+graph_body_font = 'verdana'
+graph_body_color = 'Black'
+graph_body_size = 12
+graph_show_spikes = True
 
 
-with open('../config-demo.yaml', 'w') as yaml_file:
-    yaml.dump(cfg, yaml_file)
+def generateConfig():
+    cfg = config_dict.ConfigDict()
+    cfg.results = save_directory
+    cfg.concepts = concepts_file
+    cfg.data = data_file
+    cfg.ontology = config_dict.ConfigDict()
+    cfg.ontology.location = ontology_directory
+    cfg.ontology.search = ontology_search_method
+    try:
+        umls_apikey
+    except NameError:
+        pass
+    else:
+        cfg.ontology.umls_apikey = umls_apikey
 
-print(cfg)
+    cfg.kwargs = config_dict.ConfigDict()
+    cfg.kwargs.title_font = graph_title_font
+    cfg.kwargs.title_color = graph_title_color
+    cfg.kwargs.title_size = graph_title_size
+    cfg.kwargs.text_font = graph_body_font
+    cfg.kwargs.text_color = graph_body_color
+    cfg.kwargs.text_size = graph_body_size
+    cfg.kwargs.spikes = graph_show_spikes
+
+    app_labels_dictionary = {
+        'app': 'mimic-iv-dash',
+        'version': 2.0
+    }
+    metadata_dictionary = {
+        'name': 'demo-development',
+        'labels': config_dict.FrozenConfigDict(app_labels_dictionary)
+    }
+    cfg.metadata = config_dict.FrozenConfigDict(metadata_dictionary)
+
+    with open('./config-demo.yaml', 'w') as yaml_file:
+        yaml.dump(cfg, yaml_file)
+    return cfg
+
+
+if __name__ == '__main__':
+    config = generateConfig()
+    print(config)
