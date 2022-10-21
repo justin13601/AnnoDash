cb29cd668ed3e85aefe430172dce27eaa2dceffc
Justin Xu <justin13601@hotmail.com>
2022-10-21 12:38:14 -0400

    10/21/22 - dashboard driver, loads app, layouts, and callbacks in that order to avoid circular calls

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-# https://github.com/justin13601/mimic-iv-dash/issues/6
+from dash import Dash, html, dcc, dash_table, ALL, ctx
+from dash.dependencies import State, Input, Output, ClientsideFunction
+from app import app
+from layouts import main_layout
+import callbacks
+
+app.layout = html.Div(
+    [
+        dcc.Location(id='url', refresh=False),
+        html.Div(
+            id='dashboard-content',
+            children=[],
+        )
+    ]
+)
+
+
+@app.callback(
+    Output('dashboard-content', 'children'),
+    [
+        Input('url', 'pathname'),
+    ],
+)
+def display_page(pathname):
+    if pathname == '/':
+        return main_layout
+    else:
+        return '404'
+
+
+# run app.py (MIMIC-Dash v2)
+if __name__ == "__main__":
+    # app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
+    app.run_server(port=8888, debug=True, use_reloader=False)
