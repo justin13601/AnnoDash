1f205fbc3ac313406ffd1d25a01c16188cfda958
Justin Xu <justin.xu@mail.utoronto.ca>
2022-11-11 16:17:42 -0500

    11/11/22 - moved app.server into main.py from app.py

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-Created on May 10, 2022
-@author: Justin Xu
-"""
-import dash
 from dash import Dash, html, dcc, dash_table, ALL, ctx
 from dash.dependencies import State, Input, Output, ClientsideFunction
+from app import app
+from layouts import serve_layout
+import callbacks
 
-######################################################################################################
-# APP #
-######################################################################################################
-
-app = dash.Dash(
-    __name__,
-    meta_tags=[
-        {
-            "name": "viewport",
-            "content": "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no",
-        }
-    ],
-)
-
-app.title = "MIMIC-Dash: A Clinical Terminology Annotation Dashboard"
 server = app.server
-app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div(
     [
@@ -36,7 +15,23 @@ app.layout = html.Div(
         )
     ]
 )
-# run app.py (MIMIC-Dash v2)
+
+
+@app.callback(
+    Output('dashboard-content', 'children'),
+    [
+        Input('url', 'href'),
+    ],
+)
+def display_page(href):
+    try:
+        return serve_layout()
+    except:
+        print('Something went wrong!')
+        return '404'
+
+
+# run main.py (MIMIC-Dash v2)
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
-    # app.run_server(port=8888, debug=True, use_reloader=False)
+    # app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
+    app.run_server(port=8888, debug=True, use_reloader=False)
