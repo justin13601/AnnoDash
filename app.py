92e3bbcc8b50c98cfa8fa463de7bd4d0a2ee0d5d
Justin Xu <justin.xu@mail.utoronto.ca>
2022-11-11 16:17:26 -0500

    11/11/22 - using from app import app

--- app.py
+++ app.py
@@ -1,35 +1,25 @@
-from dash import Dash, html, dcc, dash_table, ALL, ctx
-from dash.dependencies import State, Input, Output, ClientsideFunction
-from main import app
-from layouts import main_layout
-import callbacks
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-app.layout = html.Div(
-    [
-        dcc.Location(id='url', refresh=False),
-        html.Div(
-            id='dashboard-content',
-            children=[],
-        )
-    ]
-)
+"""
+Created on May 10, 2022
+@author: Justin Xu
+"""
+import dash
 
+######################################################################################################
+# APP #
+######################################################################################################
 
-@app.callback(
-    Output('dashboard-content', 'children'),
-    [
-        Input('url', 'href'),
+app = dash.Dash(
+    __name__,
+    meta_tags=[
+        {
+            "name": "viewport",
+            "content": "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no",
+        }
     ],
 )
-def display_page(href):
-    try:
-        return main_layout
-    except:
-        print('Something went wrong!')
-        return '404'
 
-
-# run app.py (MIMIC-Dash v2)
-if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
-    # app.run_server(port=8888, debug=True, use_reloader=False)
+app.title = "MIMIC-Dash: A Clinical Terminology Annotation Dashboard"
+app.config.suppress_callback_exceptions = True
