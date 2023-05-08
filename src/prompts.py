be1308afde16b04711f62766f7c250960acfa533
Justin Xu <justin.xu@mail.utoronto.ca>
2023-05-08 00:17:10 -0400

    05/03/23 - add prompts.py

---
+++ src/prompts.py
@@ -0,0 +1,3 @@
+system_prompt_template = 'You are an expert annotator with experience in clinical ontologies such as LOINC and SNOMED CT.'
+
+user_prompt_template = 'Please suggest the correct SNOMED CT code for heart rate.'
