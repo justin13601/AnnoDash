ff7c3a77e5e85ab2755aed4ac4ac831450f93382
Justin Xu <justin.xu@mail.utoronto.ca>
2023-05-15 12:49:39 -0400

    05/12/23 - keeping some physiology words filtered from top 200 stopwords

--- src/stopwords.py
+++ src/stopwords.py
@@ -37,12 +37,31 @@ def get_stopwords():
     tokens = word_tokenize(label_text)
     fdist = FreqDist(tokens)  # frequency distribution of the tokens
 
-    for word, freq in fdist.most_common(200):  # top 200 most common
-        other_words.append(word)
+    ontology_stopwords = fdist.most_common(200)  # top 200 most common
+    exclude = ['Serum', 'Entire', 'Presence', 'Ab', 'Plasma', 'oral', 'dose', 'Blood', 'Urine', 'virus', 'artery',
+               'joint', 'syndrome', 'antibody', 'neoplasm', 'spinal', 'IgG', 'skin', 'nerve', 'contrast', 'Genus',
+               'bone', 'Tissue', 'tablet', 'hour', 'blood', 'tissue', 'type', 'Salmonella', 'past', 'acid', 'muscle',
+               'disease', 'IgE', 'vein', 'bilateral', 'Titer', 'Congenital', 'eye', 'fracture', 'NAA', 'upper', 'care',
+               'Cerebral', 'antigen', 'probe', 'milliliter', 'malignant', 'Open', 'limb', 'cell', 'Primary', 'disorder',
+               'Family', 'device', 'Immunoassay', 'poisoning', 'gene', 'Ag', 'XR', 'finger', 'tendon', 'hand',
+               'parenteral', 'panel', 'anterior', 'lesion', 'PROMIS', 'cells', 'therapy', 'foot', 'valve', 'injection',
+               'ligament', 'tumor', 'IgM', 'injury', 'guidance', 'Molecular', 'IV', 'Susceptibility', 'posterior',
+               'Immunofluorescence', 'genetics', 'DNA', 'level', 'Views', 'spine', 'Body', 'thoracic', 'surface',
+               'Identifier', 'score', 'Acute', 'pressure', 'wound', 'cord', 'biopsy', 'toe', 'cutaneous',
+               'immunoglobulin', 'gland', 'head', 'CMS', 'hydrochloride', 'capsule', 'overdose', 'Excision', 'vertebra',
+               'Closed', 'Chronic', 'infection', 'lateral', 'tomography', 'assessment', 'stain']
 
-    # print(other_words)
-    STOPWORDS.extend(other_words)
+    ontology_stopwords_new = [elem for elem in ontology_stopwords if elem in exclude]
+    print(ontology_stopwords_new)
+
+    STOPWORDS.extend(ontology_stopwords_new)
+    STOPWORDS = list(map(lambda x: x.lower(), STOPWORDS))
+    STOPWORDS = list(dict.fromkeys(STOPWORDS))  # remove duplicates
 
     # executionTime = (time.time() - startTime)
     # print('Execution time in seconds: ' + str(executionTime))
+    # print(STOPWORDS)
     return STOPWORDS
+
+
+get_stopwords()
