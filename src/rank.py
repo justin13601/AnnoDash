c23caa231d3fb3bec9c1f9d2da20dac31b1a31da
Justin Xu <justin.xu@mail.utoronto.ca>
2023-05-16 00:31:42 -0400

    05/16/23 - rerank refactor, central function

--- src/rank.py
+++ src/rank.py
@@ -27,11 +27,34 @@ def get_response(model, system_prompt, user_message):
     return response
 
 
+def rerank(target, choices, method, metadata=None):
+    # GPT ranking
+    if method == 'gpt':  # GPT ranker
+        # start_time = time.time()
+        ranker = RankGPT()
+        ranker.prepare_prompt(target=target, choices=choices, metadata=metadata)
+        ranked_ids = json.loads(ranker.get_rank_results())
+        # elapsed_time = time.time() - start_time
+        # print('--------GPT Ranking Time:', elapsed_time, 'seconds--------')
+
+        # Sort datatable for display
+        return [choices[i] for i in ranked_ids]
+    elif method == 'cohere':
+        # start_time = time.time()
+        ranker = RankCohere()
+        ranker.prepare_ranker(target=target, choices=choices)
+        ranked_desc = ranker.get_rank_results()
+        # elapsed_time = time.time() - start_time
+        # print('--------Cohere Ranking Time:', elapsed_time, 'seconds--------')
+
+        # Sort datatable for display
+        return sorted(choices, key=lambda x: ranked_desc.index(x['LABEL']))
+
+
 class RankGPT:
     def __init__(self):
         # Load your API key from an environment variable or secret management service
         self.api_key = os.getenv("OPENAI_API_KEY")
-        self.response = None
         self.model = 'gpt-3.5-turbo'
         self.system_prompt = ''
         self.user_prompt = ''
@@ -51,10 +74,10 @@ class RankGPT:
         )
         return
 
-    def execute_rank(self):
+    def get_rank_results(self):
         openai.api_key = self.api_key
-        self.response = get_response(self.model, self.system_prompt, self.user_prompt)
-        return
+        response = get_response(self.model, self.system_prompt, self.user_prompt)
+        return response['choices'][0]['message']['content']
 
 
 class RankCohere:
@@ -64,17 +87,17 @@ class RankCohere:
         self.model = "rerank-multilingual-v2.0"
         self.query = ''
         self.documents = []
-        self.result = []
 
     def prepare_ranker(self, target, choices):
         self.query = target
         self.documents = [d['LABEL'] for d in choices]
         return
 
-    def execute_rank(self):
+    def get_rank_results(self):
         co = cohere.Client(self.api_key)
         rerank_hits = co.rerank(query=self.query, documents=self.documents, top_n=len(self.documents), model=self.model)
 
+        result = []
         for hit in rerank_hits:
-            self.result.append(self.documents[hit.index])
-        return
+            result.append(self.documents[hit.index])
+        return result
