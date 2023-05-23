cfb9280c4db67376c36958fade0812d77331c816
Justin Xu <justin.xu@mail.utoronto.ca>
2023-05-23 15:49:31 -0400

    05/23/23 - removed debug mode

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ def display_page(href):
 
 # run main.py
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
+    app.run_server(debug=False, host="0.0.0.0", port=8080, use_reloader=False)
